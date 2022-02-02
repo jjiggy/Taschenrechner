c9512cac6f8717923ef63b9815552ade3f354b51
jjiggy <jonatanavdv@gmail.com>
2022-02-02 17:48:00 +0100

    continious number added

--- src/Taschenrechner2.java
+++ src/Taschenrechner2.java
@@ -1,97 +1,78 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Queue;
+import java.util.List;
 import java.util.Scanner;
 
-public class Taschenrechner2 {
+public class Test {
 
-	private static Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
 
-	//public static void main(String[] args) {
-		
-		
-		public Taschenrechner2() {
+		Scanner in = new Scanner(System.in);
 		System.out.println("Bitte geben Sie Ihre Rechnung ein!!");
 		String term = in.nextLine();
 
-		//term.replaceAll("[^0-9\\+\\-\\*/()]", ""); // Ungültige Zeichen löschen
-	  //  String [] klammern = term.split("[()]");
-		//String [] operationen = term.split( "[0-9]+");
-		//int [] zahlen = Arrays.stream(term.split("[\\+\\-\\*/\\(\\)\\^]")).mapToInt(Integer::parseInt).toArray(); // Liste der
-			
-        ArrayList<String> operationsList = new ArrayList<String>();
-        
-        ArrayList<Double> numbersList = new ArrayList<Double>();
+		term.replaceAll("[^0-9\\+\\-\\*/()]", ""); // Ung�ltige Zeichen l�schen
 
-        for (int i= 0; i<operationen.length; i++) { // Überführung der Arrays in ArrayLists
-			operationsList.add(operationen[i]);}
-	    for (int i= 0; i<zahlen.length; i++) {
-				numbersList.add((double) zahlen[i]);
- 
-	   }
-	    operationsList.remove(0);
-	
-	    System.out.println(operationsList);   // am Ende entfernen
-		System.out.println(numbersList);       // am Ende entfernen
-	
-		
-		for (int i = 0; i < operationsList.size(); i++) {
-			double zwischenErgebnis;
-			switch (operationsList.get(i)) {
-			
-			case "*":
-			   
-			zwischenErgebnis = numbersList.get(i)*numbersList.get(i+1);
-			numbersList.remove(i+1);
-			operationsList.remove(i);
-			numbersList.remove(i);
-			numbersList.add(i, zwischenErgebnis);
-			i--;
-			break;
-			case "/":
-			zwischenErgebnis = numbersList.get(i)/numbersList.get(i+1);
-			numbersList.remove(i+1);
-			operationsList.remove(i);
-			numbersList.remove(i);
-			numbersList.add(i, zwischenErgebnis);
-			i--;
-			break;
-		
+		ArrayList<String> operationsList = new ArrayList<String>();
+
+		ArrayList<Double> numbersList = new ArrayList<Double>();
+
+		String continousNumber = " ";
+		boolean isContiniousNumber = false;
+		for (int i = 0; i < term.length(); i++) {
+			if (term.charAt(i) >= '0' && term.charAt(i) <= '9') {
+				isContiniousNumber = true;
+
+			} else {
+				isContiniousNumber = false;
+				operationsList.add(term.substring(i, i + 1));
 			}
-		
+			if ((isContiniousNumber == false && !continousNumber.equals("") || i == term.length() - 1)) {
+				numbersList.add(Double.parseDouble(continousNumber));
+				continousNumber = "";
+			} 
+			
 		}
-	
-		 //System.out.println(operationsList);   // am Ende entfernen
-			//System.out.println(numbersList);
-		
-			double ergebnis = numbersList.get(0);
+			for (int i = 0; i < operationsList.size(); i++) {
+				double zwischenErgebnis;
+				switch (operationsList.get(i)) {
+
+				case "*":
+
+					zwischenErgebnis = numbersList.get(i) * numbersList.get(i + 1);
+					numbersList.remove(i + 1);
+					operationsList.remove(i);
+					numbersList.remove(i);
+					numbersList.add(i, zwischenErgebnis);
+					i--;
+					break;
+				case "/":
+					zwischenErgebnis = numbersList.get(i) / numbersList.get(i + 1);
+					numbersList.remove(i + 1);
+					operationsList.remove(i);
+					numbersList.remove(i);
+					numbersList.add(i, zwischenErgebnis);
+					i--;
+					break;
+
+		}
+				
+			}
+
+		double ergebnis = numbersList.get(0);
 		for (int i = 0; i < operationsList.size(); i++) {
 			switch (operationsList.get(i)) {
-			
+
 			case "+":
-				ergebnis += numbersList.get(i+1);
+				ergebnis += numbersList.get(i + 1);
 				break;
 			case "-":
-				ergebnis -= numbersList.get(i+1);
+				ergebnis -= numbersList.get(i + 1);
 				break;
-			/*case "^":
-				Potenzieren a = new Potenzieren();
-					   
-				 break;*/
-				
-				//default:
-				//ergebnis += zahlen[i]; // bei "+" und "" (erste Zahl)
-			
+
 			}
-		
+			System.out.println("Das ergebnis lautet " + ergebnis);
 		}
-		System.out.println("Das ergebnis lautet " + ergebnis);
-	
-	
 
-
-}
-// zahlen und Operatoren in ArrayList speichern
-	//Ausgaben reihenfolge ändern
-
+	}
 }
